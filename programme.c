e8dc208c1dcd234fb7dcc87824cda84e236a1a7d
GillonB <GillonB@users.noreply.github.com>
2015-03-09 16:29:19 +0100

    Update programme.c

--- programme.c
+++ programme.c
@@ -1,1 +1,17 @@
-%insert code here
+#include <stdio.h>
+#include <stdlib.h>
+#include "dames.c"
+
+
+int main(int argc, int *argv[]){
+    struct game *Jeu = new_game(10,10);//set up game
+    print_board(Jeu); // show game
+    struct coord old = {1,3};
+    struct coord newC = {2,4};
+    struct move_seq f ={NULL,old,newC,0,NULL,0};
+    struct move mv = {NULL,&f};
+    struct coord ta;
+    int r =  is_move_seq_valid(Jeu,&f,NULL,&ta);
+    printf("%d",r);
+    free_game(Jeu); // stop and free memory from game
+}
