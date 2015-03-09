05b0226c08baebee2a3c53ed0cf051c22eb8eec6
GillonB <GillonB@users.noreply.github.com>
2015-03-09 17:09:23 +0100

    Update programme.c

--- programme.c
+++ programme.c
@@ -14,4 +14,35 @@ int main(int argc, int *argv[]){
     int r =  is_move_seq_valid(Jeu,&f,NULL,&ta);
     printf("%d",r);
     free_game(Jeu); // stop and free memory from game
+    
+    // check validity of move_seqs in moves
+    struct move_seq *iter_seq = (struct move_seq*) malloc(sizeof(struct move_seq *));
+    iter_seq = game->moves->seq;
+    struct move_seq *prev = (struct move_seq) calloc(sizeof(move_seq));
+    struct coord taken;
+    if (taken == NULL) {
+        return -1;
+    }
+    while (iter_seq != NULL)
+    {
+        int valid = is_move_seq_valid(game,iter_seq,prev,taken);
+        if (valid == 0 ) {
+            return -1; // ERREUR du mouvement ou autre
+        } else if (valid == 2) {
+                //capture
+                game->board[iter_seq->c_new.x][iter_seq->c_new.y] = game->board[iter_seq->c_old.x][iter_seq->c_old.y]; // move old pawn position to new
+                game->board[iter_seq->c_old.x][iter_seq->c_old.y] = EMPTY;
+                game->board[taken.x][taken.y] = EMPTY; // remove taken pawn
+
+
+
+        } else {
+            // simple deplacement
+            game->board[iter_seq->c_new.x][iter_seq->c_new.y] = game->board[iter_seq->c_old.x][iter_seq->c_old.y]; // move old pawn position to new
+                game->board[iter_seq->c_old.x][iter_seq->c_old.y] = EMPTY;
+            return 1;
+        }
+        prev = iter_seq;
+        iter=iter->next;
+    }
 }
